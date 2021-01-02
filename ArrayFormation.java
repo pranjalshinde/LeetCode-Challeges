92d53cb7976e1c3bd8b230b93507959ec69d48ea
Pranjal <shindepranjal818@gmail.com>
2021-01-02 21:02:52 +0530

    1st Janury 2021

---
+++ ArrayFormation.java
@@ -0,0 +1,28 @@
+public class ArrayFormation {
+    private static boolean canFormArray(int[] arr, int[][] pieces){
+        int[] index = new int[101];
+
+        for(int i = 0; i < arr.length; i++){
+            index[arr[i]] = i+1;
+        }
+        
+        for(int[] ps : pieces){
+            if(index[ps[0]] == 0){
+                return false;
+            }
+            for(int i = 1; i < ps.length; i++){
+                if(index[ps[i]] - index[ps[i-1]] != 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = {91,4,64,78};
+        int[][] pieces = {{78},{4,64},{91}};
+
+        System.out.println(canFormArray(arr,pieces));
+        
+    }
+}
